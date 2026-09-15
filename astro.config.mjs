90497029c8f486410ab44ac1df00c971cdc9d8ce
Frederic Lepy <lepy.frederic@gmail.com>
2026-09-16 00:47:07 +0900

    WIP: english version

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,4 +4,11 @@ import { defineConfig } from 'astro/config';
 export default defineConfig({
   site: 'https://fredewiik.github.io',
   base: '/sprauer-website-astro',
+  i18n: {
+    defaultLocale: 'fr',
+    locales: ['fr', 'en'],
+    routing: {
+      prefixDefaultLocale: false // Desactive le prefixe /fr/ sur la langue par défaut
+    }
+  }
 });
