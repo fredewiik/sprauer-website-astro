cdd660f7d00f31afa6c671fe5e927e501a6034ff
Frederic Lepy <lepy.frederic@gmail.com>
2026-09-15 16:57:03 +0900

    1st Deploy on Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,7 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
-import sitemap from '@astrojs/sitemap';
-
-// https://astro.build/config
 export default defineConfig({
-  integrations: [sitemap()]
+  site: 'https://fredewiik.github.io',
+  base: '/sprauer-website-astro',
 });
